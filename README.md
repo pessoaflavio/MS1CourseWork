fbfca31685186c28296292f6a554a199a871ef51
Flavio Pessoa <santf655@newschool.edu>
2018-02-28 22:45:28 +0000

    Rita.js and README.md

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 ## Major Studio 01 - Spring 2018
-Coursework and examples for Major Studio 01 - Class 2018, taught by [Richard The](http://www.richardthe.com/), assisted by [Flávio Pessoa](http://www.flaviopessoa.com.br/). This course is part of the [MS in Data Visualization](https://github.com/visualizedata) at [Parsons School of Design](https://www.newschool.edu/parsons/).
+### Coursework and examples for Major Studio 01 - Class 2018
 
+**Faculty**  [Richard The](http://www.richardthe.com/)
+
+**Teaching Assistant** [Flávio Pessoa](http://www.flaviopessoa.com.br/). 
+
+*This course is part of the [MS in Data Visualization](https://github.com/visualizedata) at [Parsons School of Design](https://www.newschool.edu/parsons/).*
+
+___
 
 These examples are code snippets and ideas using Javascript, HTML and assorted libraries. Their purpose is to demonstrate what can be done creatively with the browser and as graphic means to display data and information.
 
 Code examples by Flávio Pessoa.
 
-**01.** RiTa.js example | Documentation
+**01.** [RiTa.js example](https://github.com/mczoloft/MS1CourseWork/tree/master/examples/Rita.js) | [Documentation](https://rednoise.org/rita/reference/index.php)
--- examples/Rita.js/sketch.js
+++ examples/Rita.js/sketch.js
@@ -1,29 +1,46 @@
 function setup() {
   // put setup code here
   createCanvas(800, 600);
+  // this will load our rita function 1 frame per second
   frameRate(1);
   
 }
 
 function rita () {
-  function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
   
+  //This generates a random interger and will also round it up.
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+  //We get a random number between 0 and 4000, by passing 4000 as an argument
+  //You could also use min to define a specific range, if you want to.
   var randomGenerator = getRandomInt(4000);
   
+  //We concatenate the Met API url with our random number
   var url = 'https://collectionapi.metmuseum.org/api/collection/v1/object/' + randomGenerator;
   var metData;
   
+  //A p5 method to make a GET request 
+  //(https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
   httpGet(url, 'json', function(response) {
     
     metData = response;
     console.log(metData);
-    var keywords = RiString(metData.titles.primaryTitle);
+    
+    //Here we pick our response from the HTTP request, 
+    //and turn it into a RiTa object!
+    //The response is a JSON object, 
+    //with properties that we can pass 
+    //also to be specific in what we want
+    var keywords = RiString(metData.metadata.metaKeywords);
+    //... and then we use .words() 
+    //to have an array of all the words in our text
     var syllabus = keywords.words();
     
     console.log(syllabus);
     
+    // Iterate through our words and filter Commas to appear as gray
     for (var i = 0; i < syllabus.length; i++) {
       if (syllabus[i] === ',') 
           { fill('lightgrey')
@@ -37,6 +54,7 @@ function rita () {
 }
 
 function draw () {
+  //Calls our RiTa function
   rita();
   background('white');
 }
